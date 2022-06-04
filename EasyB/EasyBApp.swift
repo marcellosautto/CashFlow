c0151e7f5500e3f2719a478962fa04f82630a1db
marcellosautto <marcellosautto@gmail.com>
2022-06-04 16:36:12 -0700

    Initial Commit

---
+++ EasyB/EasyBApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyBApp.swift
+//  EasyB
+//
+//  Created by Marcello Sautto on 6/4/22.
+//
+
+import SwiftUI
+
+@main
+struct EasyBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
